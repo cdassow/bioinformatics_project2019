6bfc6f532e9068357bb17807ca2e43cdeac3c458
Seth Parker <sparker3@crcfe02.crc.nd.edu>
2019-10-14 14:11:33 -0400

    outlined the script a bit so it will be easy to follow. created for loops for alignment that I have not tested yet

--- MasterScript.sh
+++ MasterScript.sh
@@ -1,4 +1,21 @@
 #This script will flexibly and efficiently search genomes for the HSP70 and McrA genes and generate a summary table collating the results of all searches
-#USAGE:
+#
+############################################
+# STEP 1 : ALIGNMENT #
+############################################
+#
+#Using the muscle utility to align the desired genes. Enter your 2 desired gene types as the first two shell variables
+# bash MasterScript.sh 'hsp70*.fasta' 'mcrAgene*.fasta'
 
+for gene in ref_sequences/
+do
+  muscle -in $1 -out $1_aligned
+done
+for gene in ref_sequences/
+do
+  muscle -in $2 -out $2_aligned
+done
 
+############################################
+# STEP 2 : THE NEXT BIT #
+############################################
