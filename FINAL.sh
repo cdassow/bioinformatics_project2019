341a30690b993d9302c05ccd9c284664d68ab4c7
Jacqueline Weinrich <jweinric@crcfe02.crc.nd.edu>
2019-10-14 11:36:13 -0400

    muscle works

--- FINAL.sh
+++ FINAL.sh
@@ -1,6 +1,9 @@
 #This is the .sh file we shall use together to make a nice happy script
 #Assumption: always in bioinformatics_project2019 directory
+#Assumption: muscle in parent directory
 
+cat ./ref_sequences/mcrAgene_*.fasta > MCRA_combo.fasta
+../muscle -in MCRA_combo.fasta -out MCRA.msa
+cat ./ref_sequences/hsp70gene_*.fasta > hsp70_combo.fasta
+../muscle -in hsp70_combo.fasta -out hsp70.msa
 
-MCRA_combo=$(cat ./ref_sequences/mcrAgene_*.fasta)
-echo $MCRA_combo
