63ded54d2a6ef1354bc91220b9537f2054311477
Jacqueline Weinrich <jweinric@crcfe02.crc.nd.edu>
2019-10-14 11:46:15 -0400

    idk what's happening

--- FINAL.sh
+++ FINAL.sh
@@ -1,8 +1,8 @@
 #This is the .sh file we shall use together to make a nice happy script
 #Assumption: always in bioinformatics_project2019 directory
 #Assumption: muscle in parent directory
-
 #Convert all reference sequence files to single .fasta for both genes
+
 cat ./ref_sequences/mcrAgene_*.fasta > MCRA_combo.fasta
 ../muscle -in MCRA_combo.fasta -out MCRA.msa
 cat ./ref_sequences/hsp70gene_*.fasta > hsp70_combo.fasta
