b0dda9a680ca3f48be09b3ef590a1941e52fd687
Anna Hubert <ahubert@crcfe01.crc.nd.edu>
2019-10-14 11:17:32 -0400

    cat references

--- phresistance.sh
+++ phresistance.sh
@@ -1,3 +1,6 @@
 #This program takes the an input of FASTA files and will filter them for pH resistant Methanogens.
 
+cat ref_sequences/hsp70gene_??.fasta > hspref
+cat ref_sequences/mcrAgene_??.fasta > mcraref
+
 
