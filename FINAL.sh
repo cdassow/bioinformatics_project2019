44d96751e24b205dc4e7afffcd26ef60df4eae54
Lindsey Mortensen <lmortens@crcfe02.crc.nd.edu>
2019-10-14 11:38:13 -0400

    added comment

--- FINAL.sh
+++ FINAL.sh
@@ -2,6 +2,7 @@
 #Assumption: always in bioinformatics_project2019 directory
 #Assumption: muscle in parent directory
 
+#Convert all reference sequence files to single .fasta for both genes
 cat ./ref_sequences/mcrAgene_*.fasta > MCRA_combo.fasta
 ../muscle -in MCRA_combo.fasta -out MCRA.msa
 cat ./ref_sequences/hsp70gene_*.fasta > hsp70_combo.fasta
