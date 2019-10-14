1d5a6adff83a22ea44afb4f9aecafee0c34332eb
Jacqueline Weinrich <jweinric@crcfe02.crc.nd.edu>
2019-10-14 11:22:10 -0400

    first attempt at alignment

--- FINAL.sh
+++ FINAL.sh
@@ -1,1 +1,6 @@
 #This is the .sh file we shall use together to make a nice happy script
+#Assumption: always in bioinformatics_project2019 directory
+
+
+MCRA_combo=$(cat ./ref_sequences/mcrAgene_*.fasta)
+echo $MCRA_combo
