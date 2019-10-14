900c5353cbecf3f16a117805034dc9d8bb892618
Grace Petrosini <gpetrosi@crcfe02.crc.nd.edu>
2019-10-14 11:06:27 -0400

    outlining steps

--- candidateID.sh
+++ candidateID.sh
@@ -1,1 +1,13 @@
 #Identifying a candidate pH-resistant methanogenic Archaea
+#there are 50 candidates, in proteomes directory
+#how many HSP70, binary do they have/not have mcrA
+
+#put ref_sequences together??
+#align reference sequences muscle -in FILENAME -out OUTPUT_FILE
+#hmmbuild (alignment in --> hidden markov model out)
+#hmmsearch (to find matches)
+
+
+#for loop with grep, regex
+#does proteome__.fasta have mcrA gene
+#how many HSP70 does proteome__fasta have?
