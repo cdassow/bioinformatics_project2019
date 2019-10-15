d6a1c136cf22c0eae5ffb55875dfc8bb73beff73
Reilley Knott <rknott@crcfe01.crc.nd.edu>
2019-10-15 11:49:41 -0400

    more progress on script

--- FINAL.sh
+++ FINAL.sh
@@ -1,5 +1,4 @@
-
-his is the .sh file we shall use together to make a nice happy script
+#This is the .sh file we shall use together to make a nice happy script
 #Assumption: always in bioinformatics_project2019 directory
 #Assumption: tools in bioinformatics_project2019 directory
 #Convert all reference sequence files to single .fasta for both genes
@@ -9,13 +8,21 @@ cat ./ref_sequences/mcrAgene_*.fasta > MCRA_combo.fasta
 ./muscle -in MCRA_combo.fasta -out MCRA.msa
 cat ./ref_sequences/hsp70gene_*.fasta > hsp70_combo.fasta
 ./muscle -in hsp70_combo.fasta -out hsp70.msa
-./hmmbuild MCRA.hmm MCRA.msa
 ./hmmbuild hsp70.hmm hsp70.msa
+./hmmbuild MCRA.hmm MCRA.msa
 
 for sample in proteomes/proteome*.fasta
 do
-echo $sample
-./hmmsearch --tblout $sample.tbl MCRA.hmm $sample
+tblname=$(echo $sample | sed 's/.fasta/mcrA/')
+./hmmsearch --tblout $tblname.tbl MCRA.hmm $sample
 done
 
+for sample in proteomes/proteome*.fasta
+do
+tblname=$(echo $sample | sed 's/.fasta/hsp70/')
+./hmmsearch --tblout $tblname.tbl hsp70.hmm $sample
+done
 
+rm *.msa
+rm *.hmm
+rm *.fasta
