660136841418cbf090ce54eb0fb48303f2bd953d
gpetrosini <gpetrosi@nd.edu>
2019-10-16 11:11:01 -0400

    extracting useful info from output

--- candidateID.sh
+++ candidateID.sh
@@ -1,20 +1,12 @@
 #Identifying a candidate pH-resistant methanogenic Archaea; there are 50 candidates,
 #in proteomes directory how many HSP70, binary do they have/not have mcrA
-#USAGE bash candidateID.sh ./proteomes/proteome_*.fasta
+#USAGE bash candidateID.sh
 
-cat ./ref_sequences/mcrAgene_*.fasta | ../muscle -out alignedmcrA.fasta
+cat ./ref_sequences/mcrAgene_*.fasta | ../muscle -out alignedmcrA.fast
  ../hmmer3/bin/hmmbuild mcrA.hmm alignedmcrA.fasta
 cat ./ref_sequences/hsp70gene_*.fasta | ../muscle -out alignedhsp70.fasta
  ../hmmer3/bin/hmmbuild hsp70.hmm alignedhsp70.fasta
 
-for file in "$1"
-	do 
-	../hmmer3/bin/hmmsearch mcrA.hmm $file
-	done
-for file in "$1"                         
-        do
-        ../hmmer3/bin/hmmsearch hsp70.hmm $file      
-        done
+for file in ./proteomes/proteome*.fasta; do ../hmmer3/bin/hmmsearch mcrA.hmm $file; done
 
-#does proteome__.fasta have mcrA gene
-#how many HSP70 does proteome__fasta have?
+for file in ./proteomes/proteome*.fasta; do ../hmmer3/bin/hmmsearch hsp70.hmm $file; done
