e0ea001aa741ea6e847ef1809f8215cf7278b21e
Anna Hubert <ahubert@crcfe01.crc.nd.edu>
2019-10-16 11:19:48 -0400

    class work and programs

--- phresistance.sh
+++ phresistance.sh
@@ -1,6 +1,25 @@
 #This program takes the an input of FASTA files and will filter them for pH resistant Methanogens.
+#User must input the variable of the proteome file numbers to be analyzed. 
 
-cat ref_sequences/hsp70gene_??.fasta > hspref
-cat ref_sequences/mcrAgene_??.fasta > mcraref
+#mcrA reference sequence
+cat ref_sequences/mcrAgene_??.fasta > mcraref.fasta
+./programs/muscle -in "mcraref.fasta" -out "mcramuscle.fasta"
+./programs/hmmbuild --amino "mcrabuild.fasta" "mcramuscle.fasta"
+for proteome in ./proteomes/proteome_$@.fasta
+do
+./programs/muscle -in $proteome -out "proteomemuscle_$@.fasta"
+./programs/hmmbuild --amino "proteomebuild_$@.fasta" "proteomemuscle_$@.fasta"
+./programs/hmmsearch "mcrabuild.fasta" "proteomebuild_$@.fasta" > processedproteome$@
+done
+
+
+#hsp reference sequence
+#cat ref_sequences/hsp70gene_??.fasta > hspref.fasta
+#./progrmas/muscle -in "hspref.fasta" -out "hspmuscle.fasta"
+#./programs/hmmbuild --amino "hspbuild.fasta" "hspmuscle.fasta"
+
+#for
+#./programs/hmmsearch "hspbuild.fasta"
+
 
 
