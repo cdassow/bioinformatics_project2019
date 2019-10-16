16d78a3da6aaf06004baf94440aa09aec5cd97bb
gpetrosini <gpetrosi@nd.edu>
2019-10-16 10:53:27 -0400

    for loops; how do we read hmmsearch output

--- candidateID.sh
+++ candidateID.sh
@@ -2,12 +2,19 @@
 #in proteomes directory how many HSP70, binary do they have/not have mcrA
 #USAGE bash candidateID.sh ./proteomes/proteome_*.fasta
 
-for proteome in "$1"
-	do
-	echo $proteome
-	cat ./ref_sequences/mcrAgene_*.fasta | ../muscle | ../hmmr3/bin/hmmbuild | ../hmmer3/hmmsearch $proteome >> mcrA.txt
-	cat ./ref_sequences/hsp70gene_*.fasta | ../muscle | ../hmmr3/bin/hmmbuild | ../hmmer3/hmmsearch $proteome >> hsp70.txt
+cat ./ref_sequences/mcrAgene_*.fasta | ../muscle -out alignedmcrA.fasta
+ ../hmmer3/bin/hmmbuild mcrA.hmm alignedmcrA.fasta
+cat ./ref_sequences/hsp70gene_*.fasta | ../muscle -out alignedhsp70.fasta
+ ../hmmer3/bin/hmmbuild hsp70.hmm alignedhsp70.fasta
+
+for file in "$1"
+	do 
+	../hmmer3/bin/hmmsearch mcrA.hmm $file
 	done
+for file in "$1"                         
+        do
+        ../hmmer3/bin/hmmsearch hsp70.hmm $file      
+        done
 
 #does proteome__.fasta have mcrA gene
 #how many HSP70 does proteome__fasta have?
