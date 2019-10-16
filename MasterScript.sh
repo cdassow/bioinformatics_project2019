563a8611a5107f0e9cf4b877c6ffb13c1599af45
Seth Parker <sparker3@crcfe02.crc.nd.edu>
2019-10-16 14:11:37 -0400

    script works through hmmbuild on my machine

--- MasterScript.sh
+++ MasterScript.sh
@@ -1,11 +1,12 @@
 #This script will flexibly and efficiently search genomes for the HSP70 and McrA genes and generate a summary table collating the results of all searches
-#
+# Usage: bash MasterScript.sh
+
 ############################################
 # STEP 1 : ALIGNMENT #
 ############################################
 #
 # Using the muscle utility to align the desired genes.
-# bash MasterScript.sh 
+
 
 cat ~/Private/Biocomputing/bioinformatics/bioinformatics_project2019/ref_sequences/hsp70* > hsp70_all
 cat ~/Private/Biocomputing/bioinformatics/bioinformatics_project2019/ref_sequences/mcrA* > mcrA_all
@@ -13,5 +14,14 @@ cat ~/Private/Biocomputing/bioinformatics/bioinformatics_project2019/ref_sequenc
 ./muscle -in mcrA_all -out mcrA_all_aligned
 
 ############################################
-# STEP 2 : hmmBuild
+# STEP 2 : hmmBuild #
 ############################################
+# building a hidden markov model
+
+./ham/bin/hmmbuild hsp70_hmm hsp70_all_aligned
+./ham/bin/hmmbuild mcrA_hmm mcrA_all_aligned
+
+############################################
+# STEP 3 : hmmSearch #
+############################################
+
