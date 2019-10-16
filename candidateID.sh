94d5e3af5d44100a5d333d163f928bd84ea45e87
gpetrosini <gpetrosi@nd.edu>
2019-10-16 12:18:47 -0400

    close

--- candidateID.sh
+++ candidateID.sh
@@ -2,11 +2,25 @@
 #in proteomes directory how many HSP70, binary do they have/not have mcrA
 #USAGE bash candidateID.sh
 
+#align sequences and build HMM
 cat ./ref_sequences/mcrAgene_*.fasta | ../muscle -out alignedmcrA.fast
  ../hmmer3/bin/hmmbuild mcrA.hmm alignedmcrA.fasta
 cat ./ref_sequences/hsp70gene_*.fasta | ../muscle -out alignedhsp70.fasta
  ../hmmer3/bin/hmmbuild hsp70.hmm alignedhsp70.fasta
+
+#search through proteomes to find matches using HMM and sort to find best matches
 echo mcrA
-for file in ./proteomes/proteome*.fasta; do echo $file; ../hmmer3/bin/hmmsearch mcrA.hmm $file | grep -E "Domain search space" | sort -t 	 -k2; done
+for file in ./proteomes/proteome*.fasta
+ do 
+ echo $file:
+ ../hmmer3/bin/hmmsearch mcrA.hmm $file | grep -E "Domain search space" | tr "\n" " "
+  done
+# | sort -t ":" -k2 -b -r | head -n 5
+
 echo hsp70
-for file in ./proteomes/proteome*.fasta; do echo $file; ../hmmer3/bin/hmmsearch hsp70.hmm $file | grep -E "Domain search space" | sort -r | head -n 3; done
+for file in ./proteomes/proteome*.fasta
+ do
+ echo $file:
+ ../hmmer3/bin/hmmsearch hsp70.hmm $file | grep -E "Domain search space" | tr "\n" " "
+ done
+ #| sort -t ":" -k2 -b -r | head -n 5
