64011b6894446bbd7bdf102844dbe9512e603e09
gpetrosini <gpetrosi@nd.edu>
2019-10-15 23:19:55 -0400

    needs to be all in one script, still figuring out hmmbuild/search

--- candidateID.sh
+++ candidateID.sh
@@ -1,21 +1,11 @@
-#Identifying a candidate pH-resistant methanogenic Archaea
-#there are 50 candidates, in proteomes directory how many HSP70, binary do they have/not have mcrA
+#Identifying a candidate pH-resistant methanogenic Archaea; there are 50 candidates,
+#in proteomes directory how many HSP70, binary do they have/not have mcrA
 #USAGE bash candidateID.sh ./proteomes/proteome_*.fasta
-
-
-cat ./ref_sequences/hsp70gene_*.fasta | ../muscle | ../hmmr3/bin/hmmbuild | for proteome in $1
+for proteome in "$1"
 	do
 	echo $proteome
-        ../hmmer3/hmmsearch $proteome
-        >hsp70
-	done
-
-
-cat ./ref_sequences/mcrAgene_*.fasta | ../muscle | ../hmmr3/bin/hmmbuild| for proteome in $1
-	do 
-	echo $proteome
-	../hmmer3/hmmsearch $proteome
-	>mcrA
+	cat ./ref_sequences/mcrAgene_*.fasta | ../muscle | ../hmmr3/bin/hmmbuild | ../hmmer3/hmmsearch $proteome >> mcrA.txt
+	cat ./ref_sequences/hsp70gene_*.fasta | ../muscle | ../hmmr3/bin/hmmbuild | ../hmmer3/hmmsearch $proteome >> hsp70.txt
 	done
 
 #does proteome__.fasta have mcrA gene
