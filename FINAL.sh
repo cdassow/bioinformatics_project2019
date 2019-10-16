3558bee0b546f5b20233266505a71959e364c94c
Reilley Knott <rknott@crcfe01.crc.nd.edu>
2019-10-16 12:09:00 -0400

    final product

--- FINAL.sh
+++ FINAL.sh
@@ -1,33 +1,59 @@
-#This is the .sh file we shall use together to make a nice happy script
+#This is the bash file that will tell Dr. Jones' grad student which isolate to use
 #Assumption: always in bioinformatics_project2019 directory
 #Assumption: tools in bioinformatics_project2019 directory
+
+
 #Convert all reference sequence files to single .fasta for both genes
 #use muscle to align sequences for two genes separately 
-#use 
-
 cat ./ref_sequences/mcrAgene_*.fasta > MCRA_combo.fasta
 ./muscle -in MCRA_combo.fasta -out MCRA.msa
 cat ./ref_sequences/hsp70gene_*.fasta > hsp70_combo.fasta
 ./muscle -in hsp70_combo.fasta -out hsp70.msa
-./hmmbuild hsp70.hmm hsp70.msa
-./hmmbuild MCRA.hmm MCRA.msa
 
+
+#build hmmer profiles for hsp70 and mcrA genes
+./hmmbuild hsp70.hmm hsp70.msa
+./hmmbuild mcrA.hmm mcrA.msa
+
+
+#use hmmsearch to search the proteome genomes for the presence of mcrA sequence, based on hmmer profile created previously
+#output goes to .tbl form for ease of further operation
 for sample in proteomes/proteome*.fasta
 do
 tblname=$(echo $sample | sed 's/.fasta/mcrA/')
-./hmmsearch --tblout $tblname.tbl MCRA.hmm $sample
+./hmmsearch --tblout $tblname.tbl mcrA.hmm $sample
 done
 
+
+
+#mcrA tables for each proteome have their line count measured and a list is created of the proteomes and their respective line counts
 for table in proteomes/proteome_*mcrA.tbl
 do
 echo "$table $(cat $table | wc -l)" >> mcrAcount.txt
 done
 
+
+#13 lines is found to indicate the lack of mcrA gene, and we have no need for those proteomes; proteomes with mcrA are moved to another list
 cat mcrAcount.txt | grep -v "13" > mcrAhits.txt
 cat mcrAhits.txt | cut -d " " -f 1 | sed 's/mcrA.tbl/.fasta/' > onestosearch.txt
 
+
+#proteomes in the new file are then used in hmmsearch in order to determine how many hsp sequences they contain; again, output is in .tbl form
 for sample in $(cat onestosearch.txt )
 do
 tblname=$(echo $sample | sed 's/.fasta/hsp70/')
 ./hmmsearch --tblout $tblname.tbl hsp70.hmm $sample
 done
+
+
+#similarly to how it was done above, a new list of proteomes is created wherein each proteome is accompanied by its line count (more lines indicated more hsp70s)
+for file in proteomes/proteome_*hsp70.tbl
+do
+echo "$file $(cat $file | wc -l)" >> finallist.txt
+done
+
+
+#finallist.txt is sorted in reverse order so as to put the file with the greatest number of lines (i.e. most copies of hsp70 gene) at the top of the list
+#list contains isolates in descending order of the best candidates for further experimentation by Dr. Jones' grad student
+echo "The best isolates to move forward with are:" > FINALLIST.txt
+cat finallist.txt | sort -k2 -r | cut -d " " -f 1 | sed 's/proteomes\///' | sed 's/hsp70.tbl//' >> FINALLIST.txt
