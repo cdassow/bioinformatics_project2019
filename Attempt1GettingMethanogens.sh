c7d9abb9effc49077a52c5ae598e15480b5c41e9
Faith Taylor <ftaylor@crcfe02.crc.nd.edu>
2019-10-17 18:24:18 -0400

    adding everything

--- Attempt1GettingMethanogens.sh
+++ Attempt1GettingMethanogens.sh
@@ -1,4 +1,3 @@
-
 #cat ./ref_sequences/m*.fasta >> compiledMethaneGenes.fasta
 #cat ./ref_sequences/hsp*.fasta >> compiledpHGenes.fasta
 
@@ -11,44 +10,22 @@
 # Makes column titles in table
 echo "Proteome Number, mcrA Gene, pH Gene" > ResultsWayho.txt
 
-#MOVED THIS DOWN INTO BIG FORLOOP Searches the proteomes for the methane gene
-#for number in {01..50}
-#do
-#	../hmmer-3.2.1/bin/hmmsearch --tblout proteome_match$number.txt  methane.hmm ./proteomes/proteome_$number.fasta
-#done
-
-
-# Looks at methane matching results. Non matches have 13 lines, so we grep and get the proteome number for the hmmsearch files that have more than
-# 13 lines. These proteome numbers are for the methanogens.
-for number in {01..50}
-do
-
-	cat proteome_match$number.txt | wc -l >> proteomeLineCount$number.txt
-done
-
-
- grep -v -l '13' proteomeLineCount*.txt | tr -d proeteomeLineCount | tr -d .txt
-
-
 #HERE IS WHERE GOOD STUFF STARTS
 
 for number in {01..50}
 do
-	# Gets numbers of proteomes out of files
-	name=$(echo proteome_$number.fasta | tr -d proteome_ | tr -d .fasta)
-
 	# Searches proteomes for methane gene
 	../hmmer-3.2.1/bin/hmmsearch --tblout proteome_match$number.txt  methane.hmm ./proteomes/proteome_$number.fasta
 
-	# Some how here we need to determine if there are 13 lines and assign those proteomes a value of 0, and if there are not 13 lines assign those
-	# proteomes a value of 1. We will save these values to the variable $mcrA
+	#creates a variable for mcrA presence where 0 = no presence and 1 = a presence
+	mcrA=$(cat proteome_match$number.txt | grep -v '#' -m 1 | wc -l)
 
 	# Searches proteomes for pH gene and saves number of matches for each proteome to HSP70
   	../hmmer-3.2.1/bin/hmmsearch --tblout proteome_pHmatch$number.txt  pH.hmm ./proteomes/proteome_$number.fasta
-	HSP70=$(cat proteome_pHmatch$number.txt | grep -E -c 'WP')
-
+	HSP70=$(cat proteome_pHmatch$number.txt | grep -v '#' | tr -s ' ' '\t' | cut -f 19,20,21,22,23,24,25,26,27,28,29,30  | sort -u | wc -l)
+	
 	# Here is how we'll get the results into the final table file
-	echo "$name,$mcrA,$HSP70">> ResultsWayho.txt
+	echo "$number, $mcrA, $HSP70" >> ResultsWayho.txt
 done
 
 
