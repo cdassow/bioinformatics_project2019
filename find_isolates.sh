604a7c29ec0258b29f331d30cc3843c6746066cf
Stephen Grisoli <sgrisoli@crcfe01.crc.nd.edu>
2019-10-18 11:20:55 -0400

    Finished

--- find_isolates.sh
+++ find_isolates.sh
@@ -2,16 +2,8 @@
 #usage bash find_isolates.sh <muscle exe path> <hmmbuild exe path> <hmmsearch exe path> <name for hsp results> <name for mcrA results>
 
 #warn user of upcoming deleting files
-echo "WARNING this program  will delete any files named "hsp70gene_combined.fasta", "mcrAgene_combined.fasta", and "proteome_combined.fasta" continue? (yes/no)"
+echo "WARNING this program  will overwrite any files named "hsp70gene_combined.fasta", "mcrAgene_combined.fasta", and "final_results.txt" continue? (yes/no)"
 read bool_continue
-
-echo "Would you like the muscle alignment file saved? (yes/no)"
-read keep_musc
-
-echo "Would you like the hmmbuild file saved? (yes/no)"
-read keep_hmmbuild
-
-
 
 #switch statement based on user input
 case $bool_continue in
@@ -29,7 +21,7 @@ case $bool_continue in
 		for file in ./ref_sequences/mcrAgene_??.fasta
 		do
 		cat $file >> ./ref_sequences/mcrAgene_combined.fasta
-	cl	done
+		done
 
 
 		#allign the combined reference sequences with muscle
@@ -57,46 +49,54 @@ case $bool_continue in
 		$3 -o $4 hsp70gene.hmm ./proteomes/proteome_combined.fasta
 		$3 -o $5 mcrAgene.hmm ./proteomes/proteome_combined.fasta 
 
-		#report results location
-
-		touch final_results.txt
-		for num in {0..100}
+		#loop to count unique match in hsp70 table file and save count to array
+		i=0
+		for file in ./proteomes/*hsp70_table
 			do
-			echo "Proteome$(printf "%03d" "$num")" >> final_results.txt
+			hsp_count=$(cat $file | grep -v \# | uniq | wc -l)
+			hsp70_array[i]=$hsp_count
+			i=$i+1
 			done
+
+		#loop to count unique matches in mcrA table files and save count to array
+		j=0
+		for file in ./proteomes/*mcrA_table
+			do
+			mcrA_count=$(cat $file | grep -v \# | uniq | wc -l)
+			mcrA_array[j]=$mcrA_count
+			j=$j+1
+			done
+
+		#report results location
+		rm final_results.txt
+		echo "                 hsp70    mcrA"> final_results.txt
+                for num in {1..50}
+                        do
+                        echo ""Proteome$(printf "%03d" "$num")"        ${hsp70_array[($num-1)]}        ${mcrA_array[($num-1)]}" >> final_results.txt
+                        done
+			
 
 		
-		for file in ./proteomes/*_table
-			do
-			variable=$(cat $file | grep -v \# | uniq | wc -l)
-			echo $variable
-			for file in ./proteomes/
-			done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-		printf "Assuming hmmr and muscle executed you can find your results in this directory in files $4 and $5\n"
+		#inform user of status of saved files
+		printf "\n \n \n \n \n \n \n"
+		printf "Assuming hmmr and muscle executed you can find your results in this directory in files final_results.txt \n"
 	;;
 	no)
 	exit
 	;;
 esac
 
+#Adding user options to save certain files
+echo "Would you like the muscle alignment file saved? (yes/no)"
+read keep_musc
+
+echo "Would you like the hmmbuild file saved? (yes/no)"
+read keep_hmmbuild
+
+echo "Would you like the individual results tables saved in /proteomes?"
+read keep_tables
+
+#case staements to keep or rm the files
 case $keep_musc in
 	no)
 		rm hsp70gene.musc
@@ -118,4 +118,11 @@ case $keep_hmmbuild in
 	;;
 esac
 
-
+case $keep_tables in
+	no)
+		rm ./proteomes/*_table
+	;;
+	yes)
+		printf "\nThe individual results tables in the /proteomes child directory"
+	;;
+esac
