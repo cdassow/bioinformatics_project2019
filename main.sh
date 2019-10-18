6e9fe5dc43db97aba6953d5d91e7c1ebe9943087
Guoqiang Liu <orson@guoqiangdembp.dhcp.nd.edu>
2019-10-17 21:50:07 -0400

    align the ref seq

--- main.sh
+++ main.sh
@@ -6,3 +6,7 @@
 cat ref_sequences/hsp* > metadata/hsp_ref.fasta
 cat ref_sequences/mcrA* > metadata/mcrA_ref.fasta
 
+# align two ref sequence
+./tools/muscle -in metadata/hsp_ref.fasta -out metadata/hsp_ref.afa
+./tools/muscle -in metadata/mcrA_ref.fasta -out metadata/mcrA_ref.afa
+
