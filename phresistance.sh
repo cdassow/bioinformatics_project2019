f9e0e7da4d6b2642b3b4f985ac88d6798762e7e2
Alice Bruemmer <abruemme@crcfe01.crc.nd.edu>
2019-10-18 10:59:39 -0400

    renamed shell script

--- phresistance.sh
+++ phresistance.sh
@@ -1,5 +1,5 @@
 #This program searches fasta files for the presence of mcrA and hsp70 genes to indicate the presence of methanogens
-#usage: bash phresistance.sh 'ref_sequences/mcrAgene_*.fasta' 'ref_sequences/hsp70gene_*.fasta' './proteomes/proteome_*.fasta'
+#usage: bash phresistance.sh 'ref_sequences/mcrAgene_*.fasta' 'ref_sequences/hsp70gene_*.fasta' './proteomes/proteome_*.fasta' ./proteomes/proteome_*mcra.txt ./proteomes/proteome_*hsp70.txt
 
 #combine mcrA reference sequences into one file
 cat $1 > mcraref.fasta
@@ -35,13 +35,19 @@ processedfile=$(echo $proteome | sed 's/.fasta//g')
 done
 
 
-#thoughts for table: hsp-in the reference files, chaperone dnak is the only one that appears, so count the number of those in each hsp file for proteomes
-#chaperone dnak is written twice, so could do grep "-" | grep "molecular chaperone DnaK"
+#create table to organize data:
 
+#create table header
+echo "proteome name: mcrA count, hsp70 count" > ./summarytable.txt
 
-#must use uniq to get rid of duplicates
-# var1=(proteome_$@hsp70.txt | grep -w ">>" | cut -d " " -f 2 | wc -l >> proteome_$@hsp70_number.txt)
-# var2=(proteome_$@mcra.txt | grep -w ">>" | cut -d " " -f 2 | wc -1 >> )
+#enter data into table
+for proteome in $3
+do
+number=$(echo $proteome | sed 's/.fasta//g' | tr -d ./proteomes_)
+var1=$(cat $4 | grep ">>" | wc -l)
+var2=$(cat $5 | grep ">>" | cut -d " " -f 3-6 | uniq | wc -l)
+echo "proteome_$number: $var1, $var2" >> ./summarytable.txt
+done
 
-# echo 
-
+#create list of candidate pH-resistant methanogens
+#cat summarytable.txt | grep "1," | sort -k 3 -nr | grep -v " 0" > candidatemethanogens.txt
